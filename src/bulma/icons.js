dfa35d8f19ef26e8beb90ea73ce071b3d3841b3b
Mihai <mihai.ocneanu@gmail.com>
2019-09-23 17:13:58 +0300

    extracted menu icons

---
+++ src/bulma/icons.js
@@ -0,0 +1,4 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faPercent, faCartArrowDown, faMoneyBillAlt } from '@fortawesome/free-solid-svg-icons';
+
+library.add(faPercent, faCartArrowDown, faMoneyBillAlt);
